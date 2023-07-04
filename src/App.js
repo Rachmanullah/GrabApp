547f107d34f8f87c52361a4adf37c283701e6007
Rachmanullah <rachmanullah1@gmail.com>
2023-07-04 22:01:00 +0700

    Ovo Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet, Dimensions,StatusBar} from 'react-native';
-import { Awan } from './assets';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+} from 'react-native';
+import {Awan} from './assets';
+import OvoComponent from './component/ovoComponent/OvoComponent';
+
 class App extends Component {
   render() {
     return (
       <View>
-        <StatusBar barStyle="dark-content" translucent backgroundColor="rgba(0,0,0,0)"/>
-        <Image style={styles.header} source={Awan}/>
+        <StatusBar
+          barStyle="dark-content"
+          translucent
+          backgroundColor="rgba(0,0,0,0)"
+        />
+        <Image style={styles.header} source={Awan} />
+        <Text style={styles.greetingText}>Selamat Pagi</Text>
+        <View style={styles.wrapperOVO}>
+          <View style={styles.ViewTextOvo}>
+            <Text style={styles.textOvo}>Ovo Balance</Text>
+            <Text style={styles.textOvo}>Rp. 100.000</Text>
+          </View>
+          <OvoComponent/>
+        </View>
       </View>
     );
   }
@@ -16,10 +37,38 @@ const windowHeight = Dimensions.get('window').height;
 export default App;
 
 const styles = StyleSheet.create({
-  header : {
+  header: {
     height: 150,
     width: windowWidth,
     borderBottomLeftRadius: 50,
     borderBottomRightRadius: 50,
-  }
-})
+  },
+  greetingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    position: 'absolute',
+    fontFamily: 'Bitter-SemiBoldItalic',
+    alignSelf: 'center',
+    top: 30,
+    color: '#383838',
+  },
+  wrapperOVO: {
+    marginHorizontal: 18,
+    height: 150,
+    marginTop: -70,
+    backgroundColor: 'white',
+    elevation: 10,
+    borderRadius: 10,
+  },
+  ViewTextOvo: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 12,
+    marginTop: 10,
+  },
+  textOvo: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#383838',
+  },
+});
---
+++ src/assets/icon/index.js
@@ -0,0 +1,40 @@
+import Bike from './bike.jpg'
+import Car from './car.jpg'
+import Doctor from './doctor.jpg'
+import Food from './food.jpg'
+import More from './more.jpg'
+import NavAccountActive from './nav-account-active.jpg'
+import NavAccount from './nav-account.jpg'
+import NavHomeActive from './nav-home-active.jpg'
+import NavHome from './nav-home.jpg'
+import NavInboxActive from './nav-inbox-active.jpg'
+import NavInbox from './nav-inbox.jpg'
+import NavPaymentActive from './nav-payment-active.jpg'
+import NavPayment from './nav-payment.jpg'
+import PayIcon from './pay-icon.jpg'
+import Pulsa from './pulsa.jpg'
+import Reward from './reward.jpg'
+import Send from './send.jpg'
+import Subcribe from './subscribe.jpg'
+import Topup from './topup.jpg'
+export {
+    Bike,
+    Car,
+    PayIcon,
+    Doctor,
+    Food,
+    More,
+    NavAccount,
+    NavAccountActive,
+    NavHome,
+    NavHomeActive,
+    NavInbox,
+    NavInboxActive,
+    NavPayment,
+    NavPaymentActive,
+    Pulsa,
+    Reward,
+    Send,
+    Subcribe,
+    Topup
+}
